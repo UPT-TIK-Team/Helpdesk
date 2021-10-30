88c315eb9628d34b33e635eead109fe55556f9eb
Muhammad Fahmi prasetio <fahmiprasetio754@gmail.com>
2021-10-30 14:09:00 +0700

    Update 10.php

--- application/views/global/menus/10.php
+++ application/views/global/menus/10.php
@@ -2,3 +2,4 @@
 <li><a href="<?= BASE_URL ?>tickets/create_new"> <i class="fa fa-ticket"></i>New Ticket </a></li>
 <li><a href="<?= BASE_URL ?>tickets/my_tickets"> <i class="fa fa-list"></i>My Tickets </a></li>
 <li><a href="<?= BASE_URL ?>user/profile"> <i class="fa fa-user"></i>Profile </a></li>
+<li><a href="<?= BASE_URL ?>expertsystem/diagnose"> <i class="fa fa-globe"></i>Expert System </a></li>
