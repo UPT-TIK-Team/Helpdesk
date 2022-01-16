911f0dc7f40fa04b674c406a78a22134079f7d2c
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-16 10:52:44 +0700

    refactor(auth-footer): fix url not found

--- application/views/auth/footer.php
+++ application/views/auth/footer.php
@@ -5,9 +5,9 @@
 <script src="<?= BASE_URL ?>assets/vendor/jquery.cookie/jquery.cookie.js"> </script>
 <script src="<?= BASE_URL ?>assets/vendor/chart.js/Chart.min.js"></script>
 <script src="<?= BASE_URL ?>assets/vendor/jquery-validation/jquery.validate.min.js"></script>
-<script type="module" src="<?= BASE_URL ?>assets/js/front.js"></script>
-<script type="module" src="<?= BASE_URL ?>assets/js/library.js"></script>
-<script type="module" src="<?= BASE_URL ?>assets/js/tik-script.js"></script>
+<script type="module" src="<?= base_url('assets/js/main/front.js') ?>"></script>
+<script type="module" src="<?= base_url('assets/js/main/tik-script.js') ?>"></script>
+<script type="module" src="<?= base_url('assets/js/main/library.js') ?>"></script>
 <script src="<?= BASE_URL ?>assets/js/main/bootstrap.bundle.min.js"></script>
 <!-- Main File-->
 </body>
