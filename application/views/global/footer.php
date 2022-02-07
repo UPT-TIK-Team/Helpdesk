88762c43ab6ea9495e374e7b5542aea2ed85f107
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-02-07 12:18:30 +0700

    refactor(footer-view): remove script js import to footer

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -24,8 +24,36 @@
     </div>
   </div>
 </div>
+<script src="<?= base_url('assets/js/main/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/plugins/datatables/datatables.min.js') ?>"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<script src="<?= base_url('assets/js/main/select2.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/toastr/toastr.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/chart.js/Chart.min.js') ?>"></script>
+<script>
+  const BASE_URL = "<?= base_url() ?>";
+  $(document).ready(function() {
+    $('.side-navbar ul li a').each(function() {
+      var url = window.location.href;
+      var $this = $(this);
+      if ($this.attr('href').trim() === url) {
+        var current = $this.parent();
+        var current_parent = $this.parent().parent().siblings('a').parent();
+        current.addClass('nav-active');
+        current_parent.addClass('nav-active');
+      }
+    })
+  });
 
+  $(function() {
+    $(".loader").fadeIn();
+  });
 
+  setTimeout(function() {
+    $('.event-notification').fadeOut('fast');
+  }, 5000); // <-- time in milliseconds
+</script>
 <script src="<?= base_url('assets/vendor/jquery.cookie/jquery.cookie.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/jquery-validation/jquery.validate.min.js') ?>"></script>
 <script type="module" src="<?= base_url('assets/js/main/front.js') ?>"></script>
