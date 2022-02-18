852b5b803a2e79e638777780954669a961723688
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-02-18 10:57:59 +0700

    add missing user_guide page

---
+++ application/views/user/user_guide.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+  <div class="row mt-3">
+    <ul>
+      <li>Check helpdesk user guide in here -> <a href="https://bit.ly/HelpdeskTIK" target="_blank"><i class="fa fa-book"></i> Users Guide</a></li>
+      <li>Catalog TIK service click here -> <a href="https://docs.google.com/spreadsheets/d/14wy68XRQXP7WP-rhdqc_9tgJv8Ki4hUO/edit?usp=sharing&ouid=114839779398880711559&rtpof=true&sd=true" target="_blank"><i class="fa fa-list-alt"></i> Catalog TIK Service</a></li>
+    </ul>
+  </div>
+</div>
+</div>
