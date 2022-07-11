c1f5b95b285888822c01281f0515fe8df31fdc9a
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-07-11 14:07:12 +0700

    fix: comment some function in models

--- application/models/expertsystem/Rules_model.php
+++ application/models/expertsystem/Rules_model.php
@@ -12,10 +12,10 @@ class Rules_model extends BaseMySQL_model
   //   return parent::generateDatatable($select, $where, $join, $column, $as, $addcolumn);
   // }
 
-  public function get_rule_by_id($id)
-  {
-    return $this->db->select('problem.id as id_problem, problem.name as name_problem, symptom.id as id_symptom, symptom.name as name_symptom, mb, md')->where('rule.id', $id)->join('problem', 'rule.id_problem=problem.id')->join('symptom', 'rule.id_symptom=symptom.id')->get('rule')->row_array();
-  }
+  // public function get_rule_by_id($id)
+  // {
+  //   return $this->db->select('problem.id as id_problem, problem.name as name_problem, symptom.id as id_symptom, symptom.name as name_symptom, mb, md')->where('rule.id', $id)->join('problem', 'rule.id_problem=problem.id')->join('symptom', 'rule.id_symptom=symptom.id')->get('rule')->row_array();
+  // }
 
   public function add_rule($data)
   {
